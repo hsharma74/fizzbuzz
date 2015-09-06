e8e774a04dbe779bfb98ecde33935e2b3abbdbd6
hsharma1974 <himanshu6312@hotmail.com>
2015-09-06 07:56:04 -0700

    Fizzbuzz refactored. Working version

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,23 +1,41 @@
-$('.start-button').click( function () {
+// take input from the text-field box; parse it as an integer
+// check for NaN and call the fizzMe function with the value
+// For floats, it truncates to the nearest integer
+
+$('.submit-button').click( function () {
+	var iterations = parseInt(document.getElementById('text1').value);
+
+	if (isNaN(iterations)) {
+		alert("data is not an integer");
+	} else {
+		fizzMe(iterations);
+	}
+});
+
+
+// function: fizzMe(Integer)
+// Takes an Integer argument and counts from 1 to the 
+// argument value. Substituting 'fizz', 'buzz' or 'fizzbuzz'
+
+function fizzMe(maxVal) {
+
+	function check(n) {
+		var msg = '';
+		if (n % 3 == 0) { msg += "Fizz";}
+		if (n % 5 == 0) { msg += "Buzz";}
+		return msg || n;
+	}
 	
-	for (var i = 1; i <= 100; i++) {
-		if ( (i % 3 === 0) && (i % 5 == 0) ) {
-		  $('.fizzbuzz').append("<p>FizzBuzz</p>");
-		} else if (i % 3 === 0) {
-		  $('.fizzbuzz').append("<p>Fizz</p>");
-		} else if (i % 5 === 0) {
-		  $('.fizzbuzz').append("<p>Buzz</p>");
-		} else {
-		  $('.fizzbuzz').append("<p>" + i + "</p>");
-		}
-	
+	for (var i = 1; i <= maxVal; i++) {
+		$('.fizzbuzz').append("<p>" + check(i) + "</p>");
 	};
 
-});
+}
 
 // clear out the page for an encore
 $('.clear-button').click( function() {
 	$('.fizzbuzz').empty();
+	$('#text1').empty();
 })
 
 
